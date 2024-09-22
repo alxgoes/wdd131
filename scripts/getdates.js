caff265d357fa2f1a90605e159ead817b12fb774
alxgoes <goesalexman@gmail.com>
2024-09-21 23:01:59 -0300

    hay

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+const currentYear = new Date().getFullYear();
+document.getElementById('current-year').textContent = currentYear;
+
+const lastModified = document.lastModified;
+document.getElementById('last-modified').textContent = lastModified;
