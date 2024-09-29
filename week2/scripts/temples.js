5368deb4e241d1731fefee3996901cab4a6e0045
alxgoes <goesalexman@gmail.com>
2024-09-28 22:37:04 -0300

    asd

--- week2/scripts/temples.js
+++ week2/scripts/temples.js
@@ -1,21 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Define o ano e a data da última modificação do rodapé
     const year = new Date().getFullYear();
     document.getElementById('year').textContent = year;
     document.getElementById('last-modified').textContent = `Last Modification: ${document.lastModified}`;
 
-    // Funcionalidade do menu hamburguer
     const menuToggle = document.getElementById('menu-toggle');
     const nav = document.getElementById('nav');
 
     menuToggle.addEventListener('click', function() {
         nav.classList.toggle('active');
         if (nav.classList.contains('active')) {
-            nav.style.display = 'flex'; // Usar 'flex' para melhor layout
-            menuToggle.textContent = '❌'; // Muda para "X" ao abrir
+            nav.style.display = 'flex'; 
+            menuToggle.textContent = '❌'; 
         } else {
             nav.style.display = 'none';
-            menuToggle.textContent = '🍔'; // Retorna ao ícone de menu
+            menuToggle.textContent = '🍔'; 
         }
     });
 });
