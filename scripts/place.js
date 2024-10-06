96363ea728f05d32945ffb742e10a80911c577f9
alxgoes <goesalexman@gmail.com>
2024-10-05 21:40:12 -0300

    asds

---
+++ scripts/place.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const temperature = 10; 
+    const windSpeed = 5; 
+
+    function calculateWindChill(temp, speed) {
+        if (temp <= 10 && speed > 4.8) {
+            return Math.round(13.12 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16));
+        } else {
+            return 'N/A';
+        }
+    }
+
+    document.getElementById('wind-chill').textContent = calculateWindChill(temperature, windSpeed);
+    document.getElementById('current-year').textContent = new Date().getFullYear();
+    document.getElementById('last-modified').textContent = document.lastModified;
+});
